62348911d7878ddd349d45d04e28c0ab530ec1e6
Erick Bernal Amparano <erick082016@gmail.com>
2023-03-24 10:57:39 -0700

    feat: creeate agregar pedido page

--- renderer/components/ModalAgregarProducto.tsx
+++ renderer/components/ModalAgregarProducto.tsx
@@ -1,15 +1,26 @@
-import { CgAdd } from 'react-icons/cg'
 import { AiOutlineSearch } from 'react-icons/ai'
 import Button from './Button'
 import ModalCantidad from './ModalCantidad'
 import { useState } from 'react'
+import TablaProductos from './tables/TablaProductos'
+import { IPedidosProducto } from '../types/IPedidosProducto'
+import { IProducto } from '../types/IProducto'
 
 type ModalAgregarProductoProps = {
   closeModal: () => void
+  addProduct: (productoPedido: IPedidosProducto) => void
+  productos: IProducto[]
+  filterByNombre: (nombre: string) => void
 }
 
-const ModalAgregarProducto = ({ closeModal }: ModalAgregarProductoProps) => {
+const ModalAgregarProducto = ({
+  closeModal,
+  addProduct,
+  productos,
+  filterByNombre,
+}: ModalAgregarProductoProps) => {
   const [isOpen, setIsOpen] = useState(false)
+  const [producto, setProducto] = useState<IProducto>()
 
   const openModalCantidad = () => {
     setIsOpen(true)
@@ -18,6 +29,12 @@ const ModalAgregarProducto = ({ closeModal }: ModalAgregarProductoProps) => {
   const closeModalCantidad = () => {
     setIsOpen(false)
   }
+
+  const handleSetProducto = (producto: IProducto) => {
+    setProducto(producto)
+    openModalCantidad()
+  }
+
   return (
     <div className='absolute w-[500px] h-[630px] top-4 left-4 z-30 bg-[#1b2b44] p-4 rounded-xl space-y-4'>
       <h3 className='text-xl font-bold'>Productos</h3>
@@ -32,70 +49,26 @@ const ModalAgregarProducto = ({ closeModal }: ModalAgregarProductoProps) => {
           <input
             type='text'
             id='table-search'
-            className='block w-2/3 p-2 pl-10 text-sm text-white placeholder-gray-400 bg-gray-700 border border-gray-600 rounded-lg bg-gray-5'
+            name='table-search'
+            onChange={e => filterByNombre(e.target.value)}
+            className='block w-2/3 p-2 pl-10 text-sm text-black bg-[#ededed] border border-gray-600 rounded-lg '
             placeholder='Buscar producto...'
           />
         </div>
       </div>
-      <div className='relative overflow-x-auto h-[450px]'>
-        <table className='w-full text-sm text-left text-gray-500 dark:text-gray-400'>
-          <thead className='text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400'>
-            <tr>
-              <th scope='col' className='px-6 py-3'>
-                Id
-              </th>
-              <th scope='col' className='px-6 py-3'>
-                Nombre
-              </th>
-              <th scope='col' className='px-6 py-3'>
-                Precio
-              </th>
-              <th scope='col' className='px-6 py-3'></th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr className='bg-white border-b dark:bg-gray-900 dark:border-gray-700'>
-              <th
-                scope='row'
-                className='px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white'
-              >
-                123123
-              </th>
-              <td className='px-6 py-4'>Erick Bernal</td>
-              <td className='px-6 py-4'>$2999</td>
-              <td className='px-6 py-4'>
-                <div className='flex items-center gap-4'>
-                  <button onClick={openModalCantidad}>
-                    <CgAdd className='w-6 h-6' />
-                  </button>
-                </div>
-              </td>
-            </tr>
-            <tr className='border-b bg-gray-50 dark:bg-gray-800 dark:border-gray-700'>
-              <th
-                scope='row'
-                className='px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white'
-              >
-                123123
-              </th>
-              <td className='px-6 py-4'>Erick Bernal</td>
-              <td className='px-6 py-4'>$2999</td>
-              <td className='px-6 py-4'>
-                <div className='flex items-center gap-4'>
-                  <button onClick={openModalCantidad}>
-                    <CgAdd className='w-6 h-6' />
-                  </button>
-                </div>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+      <div className='relative overflow-x-auto h-[450px] rounded-lg'>
+        <TablaProductos addFunction={handleSetProducto} productos={productos} />
       </div>
-      <div className='flex items-center justify-between mt-4'>
-        <Button onClick={closeModal}>Cancelar</Button>
-        <Button>Registrar</Button>
+      <div className='flex items-center mt-4 '>
+        <Button onClick={closeModal}>Cerrar</Button>
       </div>
-      {isOpen && <ModalCantidad />}
+      {isOpen && (
+        <ModalCantidad
+          producto={producto}
+          closeModalCantidad={closeModalCantidad}
+          addProduct={addProduct}
+        />
+      )}
     </div>
   )
 }

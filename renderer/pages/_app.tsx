f8e3a7ffa0a6f63c61f450f95369e64d3b3c867d
Erick Bernal Amparano <erick082016@gmail.com>
2023-03-24 10:59:09 -0700

    feat: add Toast container

--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
 import type { AppProps } from 'next/app'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 import '../styles/globals.css'
 import Head from 'next/head'
@@ -10,6 +11,18 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <title>Home Camaronciza</title>
       </Head>
+      <ToastContainer
+        position='top-center'
+        autoClose={1000}
+        hideProgressBar
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme='dark'
+      />
       <Component {...pageProps} />
     </>
   )

ec14ba524040cf466c68bff0df48d3bc3a881163
Erick Bernal Amparano <erick082016@gmail.com>
2023-03-24 10:50:43 -0700

    feat: add env variables

--- renderer/next.config.js
+++ renderer/next.config.js
@@ -1,9 +1,13 @@
 module.exports = {
   webpack: (config, { isServer }) => {
     if (!isServer) {
-      config.target = 'electron-renderer';
+      config.target = 'electron-renderer'
     }
 
-    return config;
+    return config
   },
-};
+  env: {
+    API_URL: 'http://localhost:8080/api',
+    PROXY_URL: 'http://localhost:8800',
+  },
+}

a509ed6e40f30180c04fc6d2e9f1f7d1d0414829
Erick Bernal Amparano <erick082016@gmail.com>
2023-03-24 10:57:58 -0700

    feat: creaete administrar pedido page

--- renderer/pages/administrar-pedidos.tsx
+++ renderer/pages/administrar-pedidos.tsx
@@ -1,12 +1,29 @@
 import { useRouter } from 'next/router'
 import Button from '../components/Button'
-import { AiOutlineEdit, AiOutlineDelete, AiOutlineSearch } from 'react-icons/ai'
+import { AiOutlineSearch } from 'react-icons/ai'
+import { IFachadaControlador } from '../logic/IFachadaControlador'
+import { FachadaControlador } from '../logic/fachadaControlador'
+import { IPedido } from '../types/IPedido'
+import TablaPedidos from '../components/tables/TablaPedidos'
 
-function AdministrarPedidos() {
+type AdministrarPedidosProps = {
+  pedidos: IPedido[]
+}
+
+function AdministrarPedidos({ pedidos }: AdministrarPedidosProps) {
   const router = useRouter()
 
   const handleRegister = () => {
     router.push('/agregar-pedido')
+  }
+
+  const handleDelete = (id: number) => {
+    const facControlador: IFachadaControlador = new FachadaControlador()
+    facControlador.deletePedido(id).then(_ => {
+      console.log('Pedido eliminado')
+
+      router.push('/administrar-pedidos')
+    })
   }
 
   return (
@@ -14,8 +31,14 @@ function AdministrarPedidos() {
       <h1 className='text-3xl font-bold text-center'>Administrar Pedidos</h1>
       <section className='flex justify-between max-w-4xl mx-auto !mt-10'>
         <div className='flex gap-4'>
-          <Button>Buscar</Button>
-          <div className='bg-white dark:bg-gray-900'>
+          <Button
+            onClick={() => {
+              router.reload()
+            }}
+          >
+            Buscar
+          </Button>
+          <div className=''>
             <label htmlFor='table-search' className='sr-only'>
               Search
             </label>
@@ -26,7 +49,7 @@ function AdministrarPedidos() {
               <input
                 type='text'
                 id='table-search'
-                className='block p-2 pl-10 text-sm text-white placeholder-gray-400 bg-gray-700 border border-gray-600 rounded-lg w-80 bg-gray-5'
+                className='block p-2 pl-10 text-sm text-black bg-[#ededed] border border-gray-600 rounded-lg w-80 bg-gray-5'
                 placeholder='Buscar pedido...'
               />
             </div>
@@ -36,78 +59,24 @@ function AdministrarPedidos() {
       </section>
       <section className='w-full max-w-4xl mx-auto'>
         <div className='relative overflow-x-auto shadow-md sm:rounded-lg'>
-          <table className='w-full text-sm text-left text-gray-500 dark:text-gray-400'>
-            <thead className='text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400'>
-              <tr>
-                <th scope='col' className='px-6 py-3'>
-                  Id
-                </th>
-                <th scope='col' className='px-6 py-3'>
-                  Cliente
-                </th>
-                <th scope='col' className='px-6 py-3'>
-                  Total
-                </th>
-                <th scope='col' className='px-6 py-3'>
-                  Fecha de entrega
-                </th>
-                <th scope='col' className='px-6 py-3'>
-                  Lugar de entrega
-                </th>
-                <th scope='col' className='px-6 py-3'></th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr className='bg-white border-b dark:bg-gray-900 dark:border-gray-700'>
-                <th
-                  scope='row'
-                  className='px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white'
-                >
-                  123123
-                </th>
-                <td className='px-6 py-4'>Erick Bernal</td>
-                <td className='px-6 py-4'>$2999</td>
-                <td className='px-6 py-4'>22/02/2023</td>
-                <td className='px-6 py-4'>Almacen 1</td>
-                <td className='px-6 py-4'>
-                  <div className='flex items-center gap-4'>
-                    <button>
-                      <AiOutlineEdit className='w-6 h-6' />
-                    </button>
-                    <button>
-                      <AiOutlineDelete className='w-6 h-6' />
-                    </button>
-                  </div>
-                </td>
-              </tr>
-              <tr className='border-b bg-gray-50 dark:bg-gray-800 dark:border-gray-700'>
-                <th
-                  scope='row'
-                  className='px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white'
-                >
-                  123123
-                </th>
-                <td className='px-6 py-4'>Erick Bernal</td>
-                <td className='px-6 py-4'>$2999</td>
-                <td className='px-6 py-4'>22/02/2023</td>
-                <td className='px-6 py-4'>Almacen 1</td>
-                <td className='px-6 py-4'>
-                  <div className='flex items-center gap-4'>
-                    <button>
-                      <AiOutlineEdit className='w-6 h-6' />
-                    </button>
-                    <button>
-                      <AiOutlineDelete className='w-6 h-6' />
-                    </button>
-                  </div>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+          <TablaPedidos pedidos={pedidos} deletePedido={handleDelete} />
         </div>
       </section>
     </main>
   )
+}
+
+export async function getServerSideProps({ req, res }) {
+  const facControlador: IFachadaControlador = new FachadaControlador()
+
+  const pedidos = await facControlador.getPedidos()
+  //console.log(pedidos)
+
+  return {
+    props: {
+      pedidos,
+    },
+  }
 }
 
 export default AdministrarPedidos

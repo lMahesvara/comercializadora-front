4171b98be0c498e321aecd17e2fae4070680b10f
Erick Bernal Amparano <erick082016@gmail.com>
2023-03-30 09:52:59 -0700

    feat: change Button component styles

--- renderer/components/Button.tsx
+++ renderer/components/Button.tsx
@@ -3,12 +3,14 @@ import React from 'react'
 type ButtonProps = {
   children: React.ReactNode
   onClick?: () => void
+  padding?: string
 }
 
-const Button = ({ children, onClick }: ButtonProps) => {
+const Button = ({ children, onClick, padding = 'px-4 py-1 ' }: ButtonProps) => {
   return (
     <button
-      className='px-4 py-1 border rounded-lg bg-[#3c5170]'
+      className={`${padding} border rounded-lg bg-[#3c5170] hover:bg-[#2c3e4f] text-white 
+      `}
       onClick={onClick}
     >
       {children}

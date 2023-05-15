a7b39223020a06dee1b377debd4669dcd6f78864
Erick Bernal Amparano <erick082016@gmail.com>
2023-05-15 08:16:34 -0700

    feat: add field pedidos to ICliente

--- renderer/types/ICliente.ts
+++ renderer/types/ICliente.ts
@@ -1,7 +1,10 @@
+import { IPedido } from './IPedido'
+
 export interface ICliente {
   id?: number
   nombre: string
   apellido: string
   apodo: string
   adeudo?: number
+  pedidos?: IPedido[]
 }
